91f9f74d4df313c0aeff30497a767e5c6773b764
Romain Balaine <romain.balaine@epfedu.fr>
2019-10-08 20:35:33 +0200

    Security > Add rate limiter
    Set up a rate limiter on express server to limit DDOS attacks and avoid brute force authentication

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,6 @@
 import compression from 'compression';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import express from 'express';
 import logger from './lib/logger';
 import handleError from './middlewares/handle-error';
@@ -13,8 +14,16 @@ process.on('unhandledRejection', (reason, promise) => {
   logger.error('Unhandled Rejection at: Promise ', promise, reason);
 });
 
+// Set up a rate limiter to avoid brute force authentication and limit DDOS atacks
+// TODO: Adjust limit according to IT Security department recommendation
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP address to 100 requests per windowMs
+});
+
 const app = express();
 
+app.use(limiter);
 app.use(compression());
 app.use(cors());
 
